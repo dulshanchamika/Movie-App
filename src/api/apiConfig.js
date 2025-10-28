4412e4077e59242c0b74e559f7883539661dedcb
dulshanchamika <dulshanchamika2@gmail.com>
2025-10-28 23:29:35 +0530

    Apply .gitignore to remove unwanted files from repo

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '48dab66512abd5c6b39fb35c1be6a760',
+    apiKey: import.meta.env.VITE_TMDB_API_KEY,
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
